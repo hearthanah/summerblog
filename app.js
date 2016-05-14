862cd8be64d6b188a24c9f091563ac4b0cd65b04
Hanah Luong <hanahluong@Hanahs-MacBook-Pro.local>
2016-05-14 18:15:46 -0500

    initial commit

---
+++ app.js
@@ -0,0 +1,14 @@
+var express = require('express');
+var app = express();
+var path = require('path');
+
+app.get('/', function (req, res) {
+	res.sendFile(path.join(__dirname + '/static/index.html'))
+});
+
+app.listen(8000, function () {
+	console.log('Example app listening on port 3000!');
+});
+
+app.use(express.static('static'));
+app.use(express.static('bower_components/bootstrap'))
